dbb96f58de8a0162ecc440b8f16eca92b1048a23
juanes1127 <juanescardona1127@gmail.com>
2025-07-11 20:41:37 -0500

    Add files via upload

---
+++ Tallerclass6/tallerEvaluativo/src/main/java/com/mycompany/tallerevaluativo/TallerEvaluativo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallerevaluativo;
+
+
+
+/**
+ *
+ * @author Juanes Cardona
+ */
+public class TallerEvaluativo {
+
+    public static void main(String[] args) {
+        
+    }
+}
